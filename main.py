05a4a5e2eecab1475cbe8d9c2d4d7511970e0352
Melissa Liang <mlia0546@uni.sydney.edu.au>
2026-03-25 20:50:13 +1100

    idk

--- main.py
+++ main.py
@@ -14,19 +14,18 @@ class Trader:
         """Only method required. It takes all buy and sell orders for all
         symbols as an input, and outputs a list of orders to be sent."""
 
-        print("traderData: " + state.traderData)
-        print("Observations: " + str(state.observations))
-
         # Orders to be placed on exchange matching engine
         result = {}
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-
-
             
-            
-            acceptable_price = 10  # Participant should calculate this value
+            current_position = state.position[product]
+
+            past_average_price = state.own_trades[product].price / state.own_trades[product].quantity
+            current_average_price = state.observations.conversionObservations[product].bidPrice / state.observations.conversionObservations[product].quantity
+
+            acceptable_price =   # Participant should calculate this value
 
 
 
@@ -35,13 +34,13 @@ class Trader:
             if len(order_depth.sell_orders) != 0:
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
                 if int(best_ask) < acceptable_price:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
+
                     orders.append(Order(product, best_ask, -best_ask_amount))
     
             if len(order_depth.buy_orders) != 0:
                 best_bid, best_bid_amount = list[tuple[int, int]](order_depth.buy_orders.items())[0]
                 if int(best_bid) > acceptable_price:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
+    
                     orders.append(Order(product, best_bid, -best_bid_amount))
             
             result[product] = orders
